0dfd961c686b3f8aaea217653686b2a1543c0b0b
Raul Geovanny Rivera Lovo <usis031117@ugb.edu.sv>
2021-08-19 00:28:53 -0600

    Agregar archivos de proyecto.

---
+++ Visor_Raul/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnAutomatico = New System.Windows.Forms.Button()
+        Me.BtnManual = New System.Windows.Forms.Button()
+        Me.BtnSiguiente = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BtnSAlir = New System.Windows.Forms.Button()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnAutomatico
+        '
+        Me.BtnAutomatico.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnAutomatico.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnAutomatico.ForeColor = System.Drawing.Color.Red
+        Me.BtnAutomatico.Location = New System.Drawing.Point(32, 88)
+        Me.BtnAutomatico.Name = "BtnAutomatico"
+        Me.BtnAutomatico.Size = New System.Drawing.Size(109, 32)
+        Me.BtnAutomatico.TabIndex = 0
+        Me.BtnAutomatico.Text = "Automatico"
+        Me.BtnAutomatico.UseVisualStyleBackColor = False
+        '
+        'BtnManual
+        '
+        Me.BtnManual.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnManual.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnManual.ForeColor = System.Drawing.Color.Red
+        Me.BtnManual.Location = New System.Drawing.Point(32, 126)
+        Me.BtnManual.Name = "BtnManual"
+        Me.BtnManual.Size = New System.Drawing.Size(109, 29)
+        Me.BtnManual.TabIndex = 1
+        Me.BtnManual.Text = "Manual"
+        Me.BtnManual.UseVisualStyleBackColor = False
+        '
+        'BtnSiguiente
+        '
+        Me.BtnSiguiente.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnSiguiente.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSiguiente.ForeColor = System.Drawing.Color.Red
+        Me.BtnSiguiente.Location = New System.Drawing.Point(32, 161)
+        Me.BtnSiguiente.Name = "BtnSiguiente"
+        Me.BtnSiguiente.Size = New System.Drawing.Size(109, 30)
+        Me.BtnSiguiente.TabIndex = 2
+        Me.BtnSiguiente.Text = "Siguiente"
+        Me.BtnSiguiente.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(97, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(350, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Bienvenidos Ami Visor De Imagenes"
+        '
+        'Timer1
+        '
+        '
+        'BtnSAlir
+        '
+        Me.BtnSAlir.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnSAlir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSAlir.ForeColor = System.Drawing.Color.Red
+        Me.BtnSAlir.Location = New System.Drawing.Point(32, 199)
+        Me.BtnSAlir.Name = "BtnSAlir"
+        Me.BtnSAlir.Size = New System.Drawing.Size(109, 31)
+        Me.BtnSAlir.TabIndex = 9
+        Me.BtnSAlir.Text = "Salir"
+        Me.BtnSAlir.UseVisualStyleBackColor = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.Visor_Raul.My.Resources.Resources._3
+        Me.PictureBox9.Location = New System.Drawing.Point(801, 147)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(115, 83)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 13
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.Visor_Raul.My.Resources.Resources._2
+        Me.PictureBox8.Location = New System.Drawing.Point(800, 48)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(116, 83)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 12
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.Visor_Raul.My.Resources.Resources._8
+        Me.PictureBox7.Location = New System.Drawing.Point(681, 237)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(112, 83)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 11
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.Visor_Raul.My.Resources.Resources._7
+        Me.PictureBox6.Location = New System.Drawing.Point(556, 236)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(115, 84)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 10
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.Visor_Raul.My.Resources.Resources._6
+        Me.PictureBox5.Location = New System.Drawing.Point(678, 147)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(116, 83)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 8
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Visor_Raul.My.Resources.Resources._4
+        Me.PictureBox4.Location = New System.Drawing.Point(678, 48)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(116, 85)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Visor_Raul.My.Resources.Resources._5
+        Me.PictureBox3.Location = New System.Drawing.Point(556, 147)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(116, 83)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Visor_Raul.My.Resources.Resources._1
+        Me.PictureBox2.Location = New System.Drawing.Point(556, 48)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(116, 85)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(161, 74)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(359, 279)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(947, 442)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.BtnSAlir)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnSiguiente)
+        Me.Controls.Add(Me.BtnManual)
+        Me.Controls.Add(Me.BtnAutomatico)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnAutomatico As Button
+    Friend WithEvents BtnManual As Button
+    Friend WithEvents BtnSiguiente As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents BtnSAlir As Button
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+End Class
---
+++ Visor_Raul/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Private Sub BtnAutomatico_Click(sender As Object, e As EventArgs) Handles BtnAutomatico.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub BtnManual_Click(sender As Object, e As EventArgs) Handles BtnManual.Click
+        BtnSiguiente.Enabled = True
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub BtnSiguiente_Click(sender As Object, e As EventArgs) Handles BtnSiguiente.Click
+        Dim x As Integer
+        Dim random As New Random
+        x = random.Next(1, 10)
+        If x = 1 Then PictureBox1.Image = PictureBox2.Image
+        If x = 2 Then PictureBox1.Image = PictureBox3.Image
+        If x = 3 Then PictureBox1.Image = PictureBox4.Image
+        If x = 4 Then PictureBox1.Image = PictureBox5.Image
+        If x = 4 Then PictureBox1.Image = PictureBox6.Image
+        If x = 4 Then PictureBox1.Image = PictureBox7.Image
+        If x = 4 Then PictureBox1.Image = PictureBox8.Image
+        If x = 4 Then PictureBox1.Image = PictureBox9.Image
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim x As Integer
+        Dim random As New Random
+        x = random.Next(1, 10)
+        If x = 1 Then PictureBox1.Image = PictureBox2.Image
+        If x = 2 Then PictureBox1.Image = PictureBox3.Image
+        If x = 3 Then PictureBox1.Image = PictureBox4.Image
+        If x = 4 Then PictureBox1.Image = PictureBox5.Image
+        If x = 4 Then PictureBox1.Image = PictureBox6.Image
+        If x = 4 Then PictureBox1.Image = PictureBox7.Image
+        If x = 4 Then PictureBox1.Image = PictureBox8.Image
+        If x = 4 Then PictureBox1.Image = PictureBox9.Image
+
+        BtnSiguiente.Enabled = False
+    End Sub
+
+    Private Sub BtnSAlir_Click(sender As Object, e As EventArgs) Handles BtnSAlir.Click
+        If MsgBox("Desea salir de la aplicacion?", vbQuestion + vbYesNo, "pregunta") = vbYes Then
+            End
+        End If
+    End Sub
+End Class
